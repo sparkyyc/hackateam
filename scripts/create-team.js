78229058235cf29b9cced8a1fb6f7929341bbf58
Christa Sparks <christa@Christas-MacBook-Pro.local>
2018-10-30 14:23:46 -0600

    Add chip html and css

--- scripts/create-team.js
+++ scripts/create-team.js
@@ -1,5 +1,14 @@
 const url = 'http://localhost:3000'
 
-document.addEventListener('DOMContentLoades', () => {
-    
+document.addEventListener('DOMContentLoaded', () => {
+    // get event id via local storage
+
+
+    // get all skills
+    // regex to autofill search/drop down
+    // add button functionality
+
+    // on submit of form post team info 
+    // & for each skill value submited post for either association or add skill and association
+
 })
